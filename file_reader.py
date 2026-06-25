b2d085ade5fbb7169c26cf88eeac91f45783242a
sumedha <sumedhasvantmuri@gmail.com>
2026-06-25 21:39:53 +0530

    added file reader

---
+++ file_reader.py
@@ -0,0 +1,17 @@
+import json
+
+
+class FileReader:
+
+    def __init__(self):
+
+        self.file_path = "C:\Users\sumed\OneDrive\Documents\weather_monitoring\dummy.json"
+
+
+    def read(self):
+
+        with open(self.file_path, "r") as file:
+
+            raw_data = json.load(file)
+
+        return raw_data
