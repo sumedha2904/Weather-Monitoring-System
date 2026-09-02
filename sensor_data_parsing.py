a50bbedfa685e28c7000914696ea2bf6165537b7
sumedha <sumedhasvantmuri@gmail.com>
2026-09-02 12:23:34 +0530

    Update parser to handle JSON strings

--- sensor_data_parsing.py
+++ sensor_data_parsing.py
@@ -25,7 +25,6 @@ class SensorDataParser(ABC):
 class JSONParser(SensorDataParser):
 
     def parse(self, raw_data):
-        raw_data = json.loads(raw_data)
         raw_data = self.validate(raw_data)
         sensor = self.create_sensor_object(raw_data)
         return sensor
